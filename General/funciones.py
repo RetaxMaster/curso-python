35618d7e1f22ea745d6097d1b43120b9c829ed04
RetaxMaster <retaxmaster@hotmail.com>
2021-01-16 01:26:13 -0600

    Adicion de funciones al conversor

--- General/funciones.py
+++ General/funciones.py
@@ -6,7 +6,7 @@ imprimir_mensaje()
 imprimir_mensaje()
 imprimir_mensaje() """
 
-def conversacion(mensaje):
+""" def conversacion(mensaje):
     print("Hola")
     print("Cómo estás")
     print(mensaje)
@@ -15,10 +15,18 @@ def conversacion(mensaje):
 opcion = int(input("Elige una opción (1, 2, 3): "))
 
 if opcion == 1:
-   conversacion("Elegiste la opción 1")
+    conversacion("Elegiste la opción 1")
 elif opcion == 2:
     conversacion("Elegiste la opción 2")
 elif opcion == 3:
     conversacion("Elegiste la opción 3")
 else:
-    print("Escribe la opción correcta")
+    print("Escribe la opción correcta") """
+
+def suma(a, b):
+    print("Se suman dos números")
+    resultado = a + b
+    return resultado
+
+sumatoria = suma(1, 4)
+print(sumatoria)
